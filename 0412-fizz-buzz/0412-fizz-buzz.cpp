a65060bd04254ffd79e0959227c54919360b2eb5
Alvina (ale-sain) <107911392+alvinouille@users.noreply.github.com>
2023-08-18 16:27:31 +0200

    Time: 0 ms (100.00%), Space: 8 MB (24.67%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        std::vector<string> answer;
+        for (int i = 1 ; i <= n ; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                answer.push_back("FizzBuzz");
+            else if (i % 5 == 0)
+                answer.push_back("Buzz");
+            else if (i % 3 == 0)
+                answer.push_back("Fizz");
+            else
+                answer.push_back(std::to_string(i));
+        }
+        return answer;
+    }
+};
