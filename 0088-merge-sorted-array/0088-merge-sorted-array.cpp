11989024d7aae244441d1bc5cfa187174dff8332
Alvina (ale-sain) <107911392+alvinouille@users.noreply.github.com>
2023-09-18 19:19:25 +0200

    Time: 0 ms (100.00%), Space: 9.4 MB (6.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i, j, k = 0;
-        vector<int> tmp;
-        for (i = 0, j = 0; i < m && j < n ; k++)
-        {
-            tmp.push_back(nums1[i] < nums2[j] ? nums1[i++] : nums2[j++]);
-        }
-        while (k < (m + n))
-        {
-            tmp.push_back(i == m ? nums2[j++] : nums1[i++]);
-            k++;
-        }
-        for (int a = 0 ; a < tmp.size() ; a++)
-        {
-            nums1[a] = tmp[a];
-        }
+        int i = m -1;
+        int j = n -1;
+        int k = m + n - 1;
+        for (; j >= 0 && i >= 0 ; )
+            nums1[k--] = (nums2[j] > nums1[i] ? nums2[j--] : nums1[i--]);
+        for (; k >= 0 ; )
+            nums1[k--] = (j < 0 ? nums1[i--] : nums2[j--]);
     }
 };
