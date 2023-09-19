5cdc69ca595aab8596330851b1de790bbffa7c9e
Alvina (ale-sain) <107911392+alvinouille@users.noreply.github.com>
2023-09-19 19:04:02 +0200

    Time: 87 ms (85.54%), Space: 93.6 MB (10.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        std::unordered_map<int, int> map;
-        if (prices.size() == 1)
-            return 0;
-        int min = 0;
-        int max = 1;
-        map[min] = prices[0];
-        map[max] = prices[1];
-        int diff = 0;
-        if (max > min && (map[max] - map[min]) > diff)
-                diff = map[max] - map[min];
-        for (int i = 1 ; i < prices.size() - 1; i++)
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        for (int buy = 0, sell = 1; sell < prices.size(); buy++, sell++) 
         {
-            if (map[min] > prices[i])
-            {
-                min = i;
-                map[min] = prices[i];
-                max = i + 1;
-                map[max] = prices[i + 1];
-            }
-            if (map[max] < prices[i + 1])
-            {
-                max = i + 1;
-                map[max] = prices[i + 1];
-            }
-            if (max > min && (map[max] - map[min]) > diff)
-                diff = map[max] - map[min];
+            mini = min(prices[buy], mini);
+            maxi = max (prices[sell] - mini, maxi);
         }
-        return diff;
+        return (maxi > 0 ? maxi : 0);
     }
 };
