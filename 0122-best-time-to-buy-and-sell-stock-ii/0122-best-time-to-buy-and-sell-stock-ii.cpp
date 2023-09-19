01f9763829ca776a18ae46992a5bf18404a987ab
Alvina (ale-sain) <107911392+alvinouille@users.noreply.github.com>
2023-09-19 19:18:52 +0200

    Time: 4 ms (75.56%), Space: 13.3 MB (36.11%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.size() -1; i++)
+            profit += (prices[i] < prices[i + 1] ? prices[i + 1] - prices[i] : 0);
+        return profit;
+    }
+};
